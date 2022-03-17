99887c81b68e75099b9891635380e51bcc6dce72
Thilo Ettelt <thilo.ettelt@neuland-bfi.de>
2022-03-17 08:23:18 +0100

    Changed order of execution

--- ansible.sh
+++ ansible.sh
@@ -4,4 +4,15 @@ ansible-galaxy install -r requirements.yml
 sudo ansible-galaxy collection install -r requirements.yml
 ansible-galaxy collection install -r requirements.yml
 
-ansible-playbook playbook_*.yml -K #--tags "dconf" # --verbose --tags ""
+ansible-playbook playbook_packages_tumbleweed.yml
+ansible-playbook playbook_packages_docker_kubernetes_client.yml
+# interactive: ansible-playbook playbook_git.yml
+ansible-playbook playbook_filelimits.yml
+ansible-playbook playbook_aws.yml
+ansible-playbook playbook_jetbrains.yml
+# use with gnome: ansible-playbook playbook_gnome.yml
+ansible-playbook playbook_zsh.yml
+ansible-playbook playbook_sdks.yml
+ansible-playbook playbook_flatpaks.yml
+
+#Random order, use with care: ansible-playbook playbook_*.yml -K # reminder: --tags "dconf" # --verbose --tags ""
