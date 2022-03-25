463876aa96d82a31bbd88392b38f3c3703f8faa3
Thilo Ettelt <thilo.ettelt@neuland-bfi.de>
2022-03-25 13:35:18 +0100

    All playbook

--- ansible.sh
+++ ansible.sh
@@ -4,16 +4,6 @@ ansible-galaxy install -r requirements.yml
 sudo ansible-galaxy collection install -r requirements.yml
 ansible-galaxy collection install -r requirements.yml
 
-ansible-playbook playbook_packages_tumbleweed.yml
-ansible-playbook playbook_packages_docker_kubernetes_client.yml
-# interactive: ansible-playbook playbook_git.yml
-ansible-playbook playbook_filelimits.yml
-ansible-playbook playbook_aws.yml
-ansible-playbook playbook_jetbrains.yml
-ansible-playbook playbook_kde.yml
-# use with gnome: ansible-playbook playbook_gnome.yml
-ansible-playbook playbook_zsh.yml
-ansible-playbook playbook_sdks.yml
-ansible-playbook playbook_flatpaks.yml
+ansible-playbook playbook_all.yml -K
 
 #Random order, use with care: ansible-playbook playbook_*.yml -K # reminder: --tags "dconf" # --verbose --tags ""
